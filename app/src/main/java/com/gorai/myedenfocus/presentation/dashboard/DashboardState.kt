5c9d22a366136f39c7261ad042bcaa8a5da3d810
Subhajit Gorai <sbhjt-gr@users.noreply.github.com>
2024-08-11 00:38:10 +0530

    add dashboardstate

---
+++ app/src/main/java/com/gorai/myedenfocus/presentation/dashboard/DashboardState.kt
@@ -0,0 +1,16 @@
+package com.gorai.myedenfocus.presentation.dashboard
+
+import com.gorai.myedenfocus.domain.model.Session
+import com.gorai.myedenfocus.domain.model.Subject
+import androidx.compose.ui.graphics.Color
+
+data class DashboardState(
+    val totalSubjectCount: Int = 0,
+    val totalStudiedHours: String = "",
+    val totalGoalHours: String = "",
+    val subjects: List<Subject> = emptyList(),
+    val subjectName: String = "",
+    val goalStudyHours: String = "",
+    val subjectCardColors: List<Color> = Subject.subjectCardColors.random(),
+    val session: Session? = null
+)
