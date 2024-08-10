131b77c9a4c3ae7f7e2f244a36ebd3e88280dce3
Subhajit Gorai <sbhjt-gr@users.noreply.github.com>
2024-08-10 23:56:59 +0530

    new bugs added to fix later

--- app/src/main/java/com/gorai/myedenfocus/data/repository/SubjectRepositoryImpl.kt
+++ app/src/main/java/com/gorai/myedenfocus/data/repository/SubjectRepositoryImpl.kt
@@ -1,4 +1,30 @@
 package com.gorai.myedenfocus.data.repository
 
-class SubjectRepositoryImpl {
+import com.gorai.myedenfocus.data.local.SubjectDao
+import com.gorai.myedenfocus.domain.model.Subject
+import com.gorai.myedenfocus.domain.repository.SubjectRepository
+import kotlinx.coroutines.flow.Flow
+
+class SubjectRepositoryImpl(
+    private val subjectDao: SubjectDao
+): SubjectRepository {
+    override suspend fun upsertSubject(subject: Subject) {
+        subjectDao.upsertSubject(subject)
+    }
+    override fun getTotalSubjectCount(): Flow<Int> {
+        TODO("Not yet implemented")
+    }
+    override suspend fun getSubjectById(subjectId: Int): Subject? {
+        TODO("Not yet implemented")
+    }
+    override suspend fun deleteSubject(subjectInt: Int) {
+        TODO("Not yet implemented")
+    }
+    override fun getTotalGoalHours(): Flow<Float> {
+        TODO("Not yet implemented")
+    }
+    override fun getAllSubjects(): Flow<List<Subject>> {
+        TODO("Not yet implemented")
+    }
+
 }
