31a966f56adccc39b0e9b15454c6220b335f2d5a
Subhajit Gorai <sg_outlp@outlook.com>
2024-12-29 23:36:55 +0800

    minor increment update

---
+++ app/src/main/java/com/gorai/myedenfocus/presentation/task/TaskEvent.kt
@@ -0,0 +1,15 @@
+package com.gorai.myedenfocus.presentation.task
+
+import com.gorai.myedenfocus.domain.model.Subject
+import com.gorai.myedenfocus.util.Priority
+
+sealed class TaskEvent {
+    data class OnTitleChange(val title: String) : TaskEvent()
+    data class OnDescriptionChange(val description: String) : TaskEvent()
+    data class OnDateChange(val millis: Long?) : TaskEvent()
+    data class OnPriorityChange(val priority: Priority) : TaskEvent()
+    data class OnRelatedSubjectSelect(val subject: Subject) : TaskEvent()
+    data object OnIsCompleteChange : TaskEvent()
+    data object SaveTask : TaskEvent()
+    data object DeleteTask : TaskEvent()
+}
---
+++ app/src/main/java/com/gorai/myedenfocus/presentation/task/TaskState.kt
@@ -0,0 +1,15 @@
+package com.gorai.myedenfocus.presentation.task
+import com.gorai.myedenfocus.domain.model.Subject
+import com.gorai.myedenfocus.util.Priority
+
+data class TaskState(
+    val title: String = "",
+    val description: String = "",
+    val dueDate: Long? = null,
+    val isTaskComplete: Boolean = false,
+    val priority: Priority = Priority.LOW,
+    val relatedToSubject: String? = null,
+    val subjects: List<Subject> = emptyList(),
+    val subjectId: Int? = null,
+    val currentTaskId: Int? = null
+)
